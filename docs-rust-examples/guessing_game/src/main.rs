f7755caf39afef038c1d96a0ffe6c95ac5d10353
Manoj <manoj.mk27@gmail.com>
2016-09-14 14:36:52 +0530

    Guessing game refactor combinations

--- docs-rust-examples/guessing_game/src/main.rs
+++ docs-rust-examples/guessing_game/src/main.rs
@@ -3,20 +3,30 @@ extern crate rand;
 use std::io;
 use rand::Rng;
 
-fn main() {
-    println!("Guessing game !");
-
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    println!("The secret number is {}", secret_number);
-
+fn ask_for_input() -> String{
     println!("Please input your guess!");
 
     let mut guess = String::new();
+    let reader = io::stdin();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    reader.read_line(&mut guess).ok().expect("Failed to read line");
 
-    println!("You guess: {}", guess);
+    return guess;
+}
+
+fn generate_secret_number() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is {}", secret_number);
+}
+
+
+fn main() {
+    println!("Guessing game !");
+
+    generate_secret_number();
+    let guess = ask_for_input();
+
+    println!("You guessed: {}", guess);
 
 }
