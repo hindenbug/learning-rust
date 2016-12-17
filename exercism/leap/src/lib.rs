6960807a67ceec12a6792dd5195a237a9e53b379
Manoj <manoj.mk27@gmail.com>
2016-12-18 01:51:09 +0530

    Refactor

--- exercism/leap/src/lib.rs
+++ exercism/leap/src/lib.rs
@@ -1,10 +1,5 @@
 pub fn is_leap_year(year: i32) -> bool {
-
-    if divisible_by(year, 4) && (divisible_by(year, 400) || !divisible_by(year, 100)) {
-        true
-    } else {
-        false
-    }
+    divisible_by(year, 4) && (divisible_by(year, 400) || !divisible_by(year, 100))
 }
 
 fn divisible_by(a: i32, b:i32) -> bool {
